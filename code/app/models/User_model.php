ce6695f732a3ebb9429fce16f1c770ebfb67b684
Fathin Muhammad <51737398+fathinmuhammad@users.noreply.github.com>
2019-06-12 14:20:59 +0700

    Add files via upload

---
+++ code/app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php
+
+class User_model {
+    private $nama = 'Fathin Muhammad';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
---
+++ code/app/views/home/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="jumbotron mt-4">
+      <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+      <p class="lead">Halo, nama saya <?= $data['nama']; ?></p>
+      <hr class="my-4">
+      <p>Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik, tanpa ada perubahan apapun. Ia mulai dipopulerkan pada tahun 1960 dengan diluncurkannya lembaran-lembaran Letraset yang menggunakan kalimat-kalimat dari Lorem Ipsum, dan seiring munculnya perangkat lunak Desktop Publishing seperti Aldus PageMaker juga memiliki versi Lorem Ipsum.</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+
+
+</div>
